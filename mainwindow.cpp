60ff5cd39193f2a1bd9945106edd010918a166af
cambridgejames <cambridge_james@foxmail.com>
2019-11-13 00:30:00 +0800

    初步的事件绑定
    1. 使限速模块输入框的enabled属性跟随限速启用复选框的状态变化
    2. 为网站根目录浏览按钮添加点击弹出文件选择器事件
    
    已知的问题：
    1. 文件选择器窗口标题乱码
    2. 根目录的文件选择器应该改为使用目录选择器

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
@@ -12,4 +13,29 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::on_ckbSpeedLimit_stateChanged(int arg1)
+{
+	if(ui->ckbSpeedLimit->isChecked()) {
+		ui->spbMaxFlow->setEnabled(true);
+		ui->spbMaxConnect->setEnabled(true);
+	} else {
+		ui->spbMaxFlow->setEnabled(false);
+		ui->spbMaxConnect->setEnabled(false);
+	}
+}
+
+void MainWindow::on_btnRootDir_clicked()
+{
+	QFileDialog *fileDialog = new QFileDialog(this);
+	fileDialog->setWindowTitle(QStringLiteral("选择网站的根目录"));	//定义文件对话框标题
+	fileDialog->setDirectory(".");										//设置默认文件路径
+	fileDialog->setNameFilter(tr("File(*.*)"));						//设置文件过滤器
+	fileDialog->setViewMode(QFileDialog::Detail);						//设置视图模式
+
+	QStringList fileNames;
+	if (fileDialog->exec()) {
+		fileNames = fileDialog->selectedFiles();
+	}
 }
